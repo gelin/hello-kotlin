1917f15238ca76423b5b1aea9ad96ed257d6284e
Denis Nelubin <dnelubin@gmail.com>
2018-11-09 14:54:55 +0600

    Implemented hello controller.

--- src/main/kotlin/it/sevenbits/devops/hello/controllers/HelloController.kt
+++ src/main/kotlin/it/sevenbits/devops/hello/controllers/HelloController.kt
@@ -1,10 +1,19 @@
 package it.sevenbits.devops.hello.controllers
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 open class HelloController {
 
-    
+    @RequestMapping("/api/hello")
+    @ResponseBody
+    open fun hello(
+        @RequestParam(value="name", defaultValue="World") name: String
+    ): String {
+        return "Hello, $name!\n"
+    }
 
 }
